0991fd29d43ea8856cda6eeffdcc15e3751f5bfd
Arkadiusz Adamowicz <arek.adamowicz81@gmail.com>
2022-06-15 22:57:16 +0200

    update

--- app.js
+++ app.js
@@ -11,11 +11,11 @@ button.addEventListener("click", () => {
   fetch(`https://api.openweathermap.org/data/2.5/weather?q=${input.value}&appid=6747fce384ac05724f4312453dd1dd48`)
   .then(response => response.json())
   .then(data => {
-    const nameValue = data['name'];
-    const descValue = data['weather'][0]['description'];
-    const tempValue = data['main']['temp'];
-    const windValue = data['wind']['speed'];
-    const pressureValue = data['main']['pressure'];
+    const nameValue = data.name;
+    const descValue = data.weather[0].description;
+    const tempValue = data.main.temp;
+    const windValue = data.wind.speed;
+    const pressureValue = data.main.pressure;
 
     name.innerHTML = nameValue;
     desc.innerHTML = `description: ${descValue}`;
